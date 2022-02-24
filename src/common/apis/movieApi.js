6a53a03572900742f5d81a43749b3e1a890edf72
yoxuan0125 <a0920712872@gmail.com>
2022-02-24 19:22:44 +0800

    updated

--- src/common/apis/movieApi.js
+++ src/common/apis/movieApi.js
@@ -5,44 +5,50 @@ import {
 	addMovies,
 	clearList,
 	getNextPage,
+	getAllMovies,
 } from "../../Redux/movies/movieSlice";
 
 export default function useMovieSearch(category) {
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
-	const [movies, setMovies] = useState([]);
+
 	const [hasMore, setHasMore] = useState(false);
 	const dispatch = useDispatch();
 	const APIKey = "55d94f60e799bfe097c0411107134875";
 
 	const pageNumber = useSelector(getNextPage);
+	const movies = useSelector(getAllMovies);
 
 	useEffect(() => {
 		dispatch(clearList());
 	}, [category]);
 
 	useEffect(async () => {
-		setLoading(true);
-		setError(false);
-		let cancel;
-		await axios({
-			method: "GET",
-			url: `
-https://api.themoviedb.org/3/discover/${category}?api_key=${APIKey}&language=zh-TW&sort_by=popularity.desc&with_watch_monetization_types=flatrate`,
-			params: { page: pageNumber },
-			cancelToken: new axios.CancelToken((c) => (cancel = c)),
-		})
-			.then((res) => {
-				setHasMore(res.data.results.length > 0);
-				setLoading(false);
-				dispatch(addMovies(res.data.results));
-			})
-			.catch((e) => {
-				if (axios.isCancel(e)) return;
-				setError(true);
-			});
-		return () => cancel();
-	}, [pageNumber, category]);
+		{
+			{
+				setLoading(true);
+				setError(false);
+				let cancel;
+				await axios({
+					method: "GET",
+					url: `
+			https://api.themoviedb.org/3/discover/${category}?api_key=${APIKey}&language=zh-TW&sort_by=popularity.desc&with_watch_monetization_types=flatrate`,
+					params: { page: pageNumber },
+					cancelToken: new axios.CancelToken((c) => (cancel = c)),
+				})
+					.then((res) => {
+						setHasMore(res.data.results.length > 0);
+						setLoading(false);
+						dispatch(addMovies(res.data.results));
+					})
+					.catch((e) => {
+						if (axios.isCancel(e)) return;
+						setError(true);
+					});
+				return () => cancel();
+			}
+		}
+	}, [pageNumber, dispatch]);
 
-	return { loading, error, movies, hasMore };
+	return { loading, error, hasMore };
 }
