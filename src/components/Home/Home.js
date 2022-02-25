581199792706e0b4e1fd710a3fdde5bb58f704a0
yoxuan0125 <a0920712872@gmail.com>
2022-02-25 13:33:43 +0800

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,6 +13,7 @@ const Home = () => {
 
 	const [hasMore, setHasMore] = useState(false);
 	const [Top10Movies, setTop10Movies] = useState([]);
+	const [Top10TVShows, setTop10TVShows] = useState([]);
 	const APIKey = "55d94f60e799bfe097c0411107134875";
 
 	//Slider Settings
@@ -38,26 +39,23 @@ const Home = () => {
 	useEffect(() => {
 		{
 			{
-				setLoading(true);
-				setError(false);
-				let cancel;
-				axios({
-					method: "GET",
-					url: `
-			https://api.themoviedb.org/3/discover/movie?api_key=${APIKey}&language=zh-TW&sort_by=popularity.desc&with_watch_monetization_types=flatrate`,
-					params: { page: 1 },
-					cancelToken: new axios.CancelToken((c) => (cancel = c)),
-				})
-					.then((res) => {
-						setHasMore(res.data.results.length > 0);
+				const url = (c) =>
+					`https://api.themoviedb.org/3/discover/${c}?api_key=${APIKey}&language=zh-TW&sort_by=popularity.desc&with_watch_monetization_types=flatrate`;
+
+				let endpoints = [url("tv"), url("movie")];
+
+				axios
+					.all(endpoints.map((endpoint) => axios.get(endpoint)))
+					.then((data) => {
+						setHasMore(data[0].data.results.length > 0);
 						setLoading(false);
-						setTop10Movies(res.data.results.slice(0, 10));
+						setTop10TVShows(data[0].data.results.slice(0, 10));
+						console.log(data[0]);
+						setTop10Movies(data[1].data.results.slice(0, 10));
 					})
 					.catch((e) => {
-						if (axios.isCancel(e)) return;
-						setError(true);
+						console.log(e);
 					});
-				return () => cancel();
 			}
 		}
 	}, []);
@@ -77,6 +75,16 @@ const Home = () => {
 				<div className="carousel">
 					<Slider {...settings}>
 						{Top10Movies.map((item, index) => {
+							return (
+								<MovieCard data-index={index} key={item.id} item={item} index={index} />
+							);
+						})}
+					</Slider>
+				</div>
+				<div className="column-header">TOP10 熱門電影</div>
+				<div className="carousel">
+					<Slider {...settings}>
+						{Top10TVShows.map((item, index) => {
 							return (
 								<MovieCard data-index={index} key={item.id} item={item} index={index} />
 							);
