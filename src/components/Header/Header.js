a1d3ee94b5ba91f16ee84bdf8e0ec83baeef7130
yoxuan0125 <a0920712872@gmail.com>
2022-03-01 23:43:34 +0800

    RWD update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import user from "../images/user.png";
+import { FaBars } from "react-icons/fa";
 import "./Header.css";
 
 const Header = () => {
+	const [navOpen, setNavOpen] = useState(false);
+
+	const navHandler = (e) => {
+		e.preventDefault();
+		setNavOpen(!navOpen);
+	};
+
 	return (
 		<div className="header">
 			<div className="nav-wrapper">
-				<Link to="/" className="logo">
-					<h1>MOVIES</h1>
-					<p>.com</p>
+				<Link to="/" className="nav">
+					<div className="logo">
+						<h1>MOVIES</h1>
+						<p>.com</p>
+					</div>
+					<div className={navOpen ? `linksOpen` : `links`}>
+						<ul className="category">
+							<Link
+								to="movie"
+								onClick={() => {
+									setNavOpen(!navOpen);
+								}}
+							>
+								<li>熱門電影</li>
+							</Link>
+							<Link
+								to="tv"
+								onClick={() => {
+									setNavOpen(!navOpen);
+								}}
+							>
+								<li>電視節目</li>
+							</Link>
+							<li>更多</li>
+						</ul>
+					</div>
+					<button className="nav-btn" onClick={navHandler}>
+						<FaBars />
+					</button>
+					<div className="user-img">
+						<a href="/user">
+							<img src={user} alt="user" />
+						</a>
+					</div>
 				</Link>
-				<div className="links">
-					<ul>
-						<Link to="movie">
-							<li>熱門電影</li>
-						</Link>
-						<Link to="tv">
-							<li>電視節目</li>
-						</Link>
-
-						<li>更多</li>
-					</ul>
-				</div>
-			</div>
-
-			<div className="user-img">
-				<a href="/user">
-					<img src={user} alt="user" />
-				</a>
 			</div>
 		</div>
 	);
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -36,6 +36,36 @@ const Home = () => {
 		slidesToScroll: 1,
 		prevArrow: <SlickArrowLeft />,
 		nextArrow: <SlickArrowRight />,
+		responsive: [
+			{
+				breakpoint: 1600,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 1300,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 1000,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
 	};
 
 	//Get TOP10 movies
